5f1736b5c0c6eb5caa80f852f14c13be6403812f
Juan Petterson <juan.petterson@gmail.com>
2019-06-09 19:39:09 -0300

    First commit

---
+++ src/config/StatusBarConfig.js
@@ -0,0 +1,5 @@
+import { StatusBar } from 'react-native';
+import { colors } from '~/styles';
+
+StatusBar.setBackgroundColor(colors.background);
+StatusBar.setBarStyle('light-content');
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+
+import store from './store';
+
+import App from './App';
+
+const Root = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+export default Root;
